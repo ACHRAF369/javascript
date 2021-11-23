b0a000de31c49606962105b277e2a4dcf1c4569e
achraf369 <achraflahmam2@gmail.com>
2021-11-24 00:08:25 +0100

    add eval function

--- calcolator/index.js
+++ calcolator/index.js
@@ -1,15 +1,21 @@
 //*************variables************//
 
 var scren = document.getElementById("screen");
+var num ;
 
 function display(x){
     
     scren.value += x;
  
 }
+
 function clearScreen(){
     scren.value = "";
 
+}
+
+function calcolator(){
+    scren.value = eval(scren.value);
 }
 
 
