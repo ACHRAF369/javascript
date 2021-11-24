a6d883e5e1b2d7b46e08e447865e86334777a922
achraf369 <achraflahmam2@gmail.com>
2021-11-24 01:00:48 +0100

    add function precedNum whit method substring

--- calcolator/index.js
+++ calcolator/index.js
@@ -2,11 +2,15 @@
 
 var scren = document.getElementById("screen");
 var num ;
+var char;
 
 function display(x){
     
     scren.value += x;
- 
+    num = scren.value.length;
+   char = x;
+    // console.log( "total: "+ num);
+   
 }
 
 function clearScreen(){
@@ -16,6 +20,10 @@ function clearScreen(){
 
 function calcolator(){
     scren.value = eval(scren.value);
+}
+function precedNum(){
+    preced = scren.value.substring(num-2,num-1)
+    
 }
 
 
