e9f46307335cd040ce4d15271eaa7d742aa119fc
achraf369 <achraflahmam2@gmail.com>
2021-11-29 01:44:11 +0100

    finish javascript function and add code clean

--- calcolator/index.js
+++ calcolator/index.js
@@ -1,71 +1,87 @@
-//*************variables************//
+let scren = document.getElementById("screen");
+var num;
 
+let result=""  ;
+let curchar;
+let opera1;
+var operaT;
+let op;
 
-var scren = document.getElementById("screen");
-var num ;
-var curchar;
-var operation = ['+','-','*','/',];
+let opera2;
+let operation = ["+", "-", "*", "/"];
 
-function display(x){
-    
-    scren.value += x;
-    num = scren.value.length;
+function opera(b){
+     opera1 = scren.value.substring(0, num);
+     op = opera1.length
+     operaT = b ;
+
+   
+
+}
+
+function display(x) {
+   scren.value += x;
+   num = scren.value.length;
    curchar = x;
-    // console.log( "total: "+ curchar);
-    precedNum()
-   
-   
-   
+ 
+  precedNum();
 }
 
-function clearScreen(){
-    scren.value = "";
+function clearScreen() {
+  scren.value = "";
 
 }
-// function e(){
+
+
+function calcolator() {
+
+let opera2 = scren.value.substring(op+1, num);
+
+if(operaT == '*' ){
+    result = opera1 * opera2;
+ 
+
+}
+else if(operaT =='+'){
     
-//    var h=4;
-   
-//    return () => {
+      result =parseInt(opera1)  + parseInt( opera2);
+    
 
-//        console.log(h);
-
-//    }
-
-
-// }
-// e();
-
-function calcolator(){
-    scren.value = eval(scren.value);
 }
-function precedNum(){
-    prechar = scren.value.substring(num-2,num-1);
-    checkNum()
+else if(operaT == '-'){
+      result = opera1 - opera2;
+     
     
 }
-function checkNum(){
-
-     if(operation.includes(prechar) && operation.includes(curchar)){
-         if(prechar == curchar){
-          deletChar()
-          // 
-        }else{
-
-             scren.value = scren.value.slice(0, num-2) + scren.value.slice(num-1);
-
-        //   scren.value = 
-        //   console.log(num.substr(0,num) + scren.value.substr(num+1));
-             
-             
-
-        }
-    }
-}
-function deletChar(){
-    scren.value = scren.value.substring(0,num-1);
+else if(operaT == '/'){
+      result = opera1 / opera2;
+  
 }
 
+scren.value = result;
+operaT = "";
 
 
 
+}
+function precedNum() {
+  prechar = scren.value.substring(num-2, num-1);
+  checkNum();
+}
+function checkNum() {
+  if (operation.includes(prechar) && operation.includes(curchar)) {
+    if (prechar == curchar) {
+      deletChar();
+    } else {
+      scren.value = scren.value.slice(0, num - 2) + scren.value.slice(num - 1);
+
+      
+  }
+
+}
+function deletChar() {
+  scren.value = scren.value.substring(0, num - 1);
+}
+
+
+}
