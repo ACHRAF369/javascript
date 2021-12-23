151896599a0916da607cd7b23d39190127f0e934
achraf lahmam <achraflahmam2@gmail.com>
2021-12-23 12:25:55 +0100

    solving error white install dotenv

--- YoutubeToMp3/index.js
+++ YoutubeToMp3/index.js
@@ -1,13 +1,14 @@
 //required packages
 const express = require("express");
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
+
 require("dotenv").config();
 
 //create the express server
 const app = express();
 
 //server port number
-const PORT = process.env.PORT || 3030;
+const PORT =  3030;
 
 //start the server 
 app.listen(PORT, () => {
