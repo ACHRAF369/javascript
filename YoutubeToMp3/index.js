71831501b6f441fb56fcd1664ae1ea5696bd0df7
achraf lahmam <achraflahmam2@gmail.com>
2021-12-24 11:11:12 +0100

    file ejs it's work

--- YoutubeToMp3/index.js
+++ YoutubeToMp3/index.js
@@ -1,27 +1,29 @@
+
 //required packages
 const express = require("express");
-const fetch = require("node-fetch");
-
+// const fetch = require("node-fetch");
 require("dotenv").config();
 
 //create the express server
 const app = express();
 
 //server port number
-const PORT =  3030;
-
+const PORT = 3030;
 
 app.set("view engine", "ejs");
+
 app.use(express.static("public"))
 
 app.use(express.urlencoded({
     extended:true
+
 }))
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.render("index")
+
 })
 
 app.post("/", (req, res) => {
